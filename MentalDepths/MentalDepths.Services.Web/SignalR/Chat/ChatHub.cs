bcb00b3bf60e5a5dfb1f66c66fdedb1164a06d1d
Georgi <gogotin5@gmail.com>
2023-07-16 12:03:27 +0300

    js codes are ruining my professionalism but signalr is worth it

---
+++ MentalDepths/MentalDepths.Services.Web/SignalR/Chat/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿
+namespace MentalDepths.Services.Web.SignalR.Chat
+{
+    using Microsoft.AspNetCore.SignalR;
+    public class ChatHub:Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
---
+++ MentalDepths/MentalDepths.Web.ViewModels/Web/BookApointementVM.cs
@@ -0,0 +1,19 @@
+﻿using MentalDepths.Data.Models;
+
+namespace MentalDepths.Web.ViewModels.Web
+{
+    public class BookApointementVM
+    {
+        public Guid SpecialistId { get; set; }
+        public Specialist Specialist{ get; set; }
+        public string ImageURLSpecialist { get; set; }
+
+        public Guid UserId { get; set; }
+        public ApplicationUser User { get; set; }
+        public string ImageURLUser { get; set; }
+
+        public string Address { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
